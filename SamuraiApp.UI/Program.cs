0aba3c01ea5c20251099e7701563e0cedc816e7b
Barna Biro <barna.biro@lateral-inc.com>
2021-05-31 09:20:06 +0300

    initial commit

---
+++ SamuraiApp.UI/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Data;
+using SamutaiApp.Domain;
+
+namespace SamuraiApp.UI
+{
+    class Program
+    {
+        private static SamuraiContext _context = new();
+
+        private static void Main(string[] args)
+        {
+            GetSamurais("Before Add:");
+            AddSamurai();
+            GetSamurais("After Add:");
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Sampson" };
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
+        }
+        private static void GetSamurais(string text)
+        {
+            var samurais = _context.Samurais
+                .TagWith("ConsoleApp.Program.GetSamurai method")
+                .ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
+        }
+    }
+}
